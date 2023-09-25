b32c0079f924b8a08d07a5dec7b711558ae363b0
felippemafdiasferreira <129065606+felippemafdiasferreira@users.noreply.github.com>
2023-09-25 08:49:49 -0300

    Add files via upload

---
+++ js/JS main.js
@@ -0,0 +1,12 @@
+// Criar variáveis no javascript
+// var letra = '0'; var é uma versão em desuso
+// const letra = 'D';
+// console.log(letra);
+//Iniciando uma variável que representa um jogador
+let letra = 'X';
+
+function joga() {
+    let casaClicada = document.getElementById(cel).innerHTML;
+    // inerHTML - injeta tags HTML na página
+    if(casaClicada == "X")
+}
